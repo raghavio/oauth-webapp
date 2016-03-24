d3396bd26107b07e6e298f0cf90142f5a326103b
Raghav Sharma <raghav_ftw@hotmail.com>
2016-03-24 09:22:58 +0530

    Getting access token

--- redcarpet/oauth.py
+++ redcarpet/oauth.py
@@ -1,3 +1,4 @@
+import requests
 import urllib
 
 facebook = {
@@ -7,13 +8,22 @@ facebook = {
     "redirect_uri": "http://localhost:5000/oauth/facebook",
     "scope": "email",
     "login_url": "https://www.facebook.com/dialog/oauth",
-    "login_params": ["client_id", "redirect_uri", "scope"]
+    "login_params": ["client_id", "redirect_uri", "scope"],
+    "token_url": "https://graph.facebook.com/v2.3/oauth/access_token",
+    "token_params": ["client_id", "redirect_uri", "client_secret"],
 }
 
 
 def get_oauth_provider(provider):
     if provider == "facebook":
         return facebook
+
+
+def get_access_token(provider, code):
+    provider_info = get_oauth_provider(provider)
+    url = create_url(provider_info['token_url'], provider_info['token_params'], provider_info)
+    resp = requests.get(url, params={"code": code})
+    return resp.json()
 
 
 def create_url(url, params, provider_info):
--- redcarpet/views.py
+++ redcarpet/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session
 from redcarpet import app
 import oauth
 
@@ -14,3 +14,12 @@ def login(provider):
     login_url = oauth.create_url(provider_info['login_url'], provider_info['login_params'], provider_info)
     return redirect(login_url)
 
+
+@app.route("/oauth/<provider>")
+def oauth_callback(provider):
+    if "code" in request.args:
+        code = request.args["code"]
+        token_data = oauth.get_access_token(provider, code)
+        token = token_data['access_token']
+    elif "error" in request.args:
+        pass
